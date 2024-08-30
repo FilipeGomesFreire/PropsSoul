6059e9e33cf9a9e695556cc3f77a55eda194d616
FilipeGomesFreire <filipegomesfreire@gmail.com>
2024-08-29 21:31:57 -0300

    criação do projeto

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="mobile-screen">
+        <h1>Mobile Screen</h1>
+        <p>This is a responsive mobile screen.</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
